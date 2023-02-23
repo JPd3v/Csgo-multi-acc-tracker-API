bb9f89b5bcbd2370764b3d1d654392eda16885ce
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-02-23 05:59:51 -0300

    adds project initial config and dependencies

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// cors confing
+const whitelist = process.env.WHITELISTED_ORIGINS as unknown as Array<string>;
+
+const corsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (error: Error | null, succes?: boolean) => void
+  ) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,PUT,POST,DELETE",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+const mongoDB = process.env.MONGODB_URL as string;
+mongoose.set("strictQuery", false);
+mongoose.connect(mongoDB);
+
+app.listen(PORT, () => console.log(`server listening on ${PORT}`));
