86a657762b3dfd986b9396f649b3f026eb70a062
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-03-22 09:20:14 -0300

    change valdiation error json response key name

--- src/middlewares/validationErrors.ts
+++ src/middlewares/validationErrors.ts
@@ -8,7 +8,7 @@ export default function validationErrors(
 ) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({ message: errors.array() });
+    return res.status(422).json({ errors: errors.array() });
   }
 
   return next();
